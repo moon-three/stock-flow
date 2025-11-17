2cc4715e56875f83dce314502bf79b0c67ace067
moon-three <moondal366@gmail.com>
2025-11-18 01:11:12 +0900

    chore: restructure project to multi-module

---
+++ stock-online/src/main/java/com/seohee/online/StockOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.seohee.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockOnlineApplication.class, args);
+    }
+
+}
