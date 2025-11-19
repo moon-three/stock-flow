a5bd96f91d240403817ea90ce01f1ccad63169ff
moon-three <moondal366@gmail.com>
2025-11-19 14:48:39 +0900

    feat(domain): OrderProduct Entity 추가 - OrderProduct는 Order/Product에 각각 다:1 관계 - 주문 내 상품 정보를 개별적으로 관리

---
+++ stock-domain/src/main/java/com/seohee/domain/entity/OrderProduct.java
@@ -0,0 +1,21 @@
+package com.seohee.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Long quantity;
+    private Long price;
+}
